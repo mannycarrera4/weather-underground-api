3a6aa6071e7e912303737cff03dae1366b2d9e06
mannycarrera4 <manuel.carrera.91@gmail.com>
2014-09-24 21:33:45 -0500

    set up underground api and getting nashville's temp, also created a div and rendered texted using jquery with the data

--- myscript.js
+++ myscript.js
@@ -1,1 +1,17 @@
 console.log("This is my javascript file");
+
+$(document).ready(function($) {
+    var test = $("#test");
+    console.log("TEST ",test);
+    $.ajax({
+        url : "http://api.wunderground.com/api/27712612aaec568a/geolookup/conditions/q/TN/Nashville.json",
+        dataType : "jsonp",
+    }).done(function(response) {
+        var location = response.location.city;
+        var temp_f = response.current_observation.temp_f;
+        
+        test.text(location + ": " + temp_f);
+
+        console.log(response);
+    });
+});
