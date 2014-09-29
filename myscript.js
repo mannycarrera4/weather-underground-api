47d4b4dededaa467241b1dbdb916980edcb15da3
mannycarrera4 <manuel.carrera.91@gmail.com>
2014-09-29 09:25:38 -0500

    played around with the api and added an array to the ui

--- myscript.js
+++ myscript.js
@@ -4,14 +4,26 @@ $(document).ready(function($) {
     var test = $("#test");
     console.log("TEST ",test);
     $.ajax({
-        url : "http://api.wunderground.com/api/27712612aaec568a/geolookup/conditions/q/TN/Nashville.json",
+        url : "http://api.wunderground.com/api/27712612aaec568a/forecast/q/CA/San_Francisco.json",
         dataType : "jsonp",
     }).done(function(response) {
-        var location = response.location.city;
-        var temp_f = response.current_observation.temp_f;
-        
-        test.text(location + ": " + temp_f);
+        var forcastDays = response.forecast.simpleforecast.forecastday;
 
+        // Loop through each day and grab the conditions plus temp in celsius 
+        for (var i=0; i < forcastDays.length; i++) {
+            var thisDaysForecast = forcastDays[i];
+            var example = $('<div class="example">'+thisDaysForecast.high.celsius+' '+thisDaysForecast.conditions+'</div>');
+            test.append(example);
+        }
+
+        // var datePretty = response.forecast.simpleforecast.forecastday[0].date.pretty;
+        // console.log("data: ",datePretty);
+        // inserts text into an existing html element
+        // test.text(datePretty);
         console.log(response);
+
+        // Added html element using jquery 
+        // var example = $('<div class="example">'+datePretty+' This is my example</div>');
+        // test.append(example);
     });
 });
